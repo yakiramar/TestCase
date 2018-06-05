6bff0db551c094cc7268c72eff245de7cdd47658
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2018-06-05 13:30:26 +0300

    Update TestCase.hpp

--- TestCase.hpp
+++ TestCase.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ostream>
 #include <assert.h>     /* assert */
 using namespace std;
 
@@ -7,20 +8,40 @@ template<typename T>
 class TestCase{
     
     private:
+    string s; 
+    int num;  //number of test
     
     
     public:
-    template <typename T> T& check_equal(T& a, T& b); // בודקת אם שני הארגומנטים שלה שווים, אם לא - מדפיסה הודעת שגיאה
-    template <typename T> T& check_different(T& a, T& b); // בודקת אם שני הארגומנטים שונים, כנ"ל
-    template <typename T> T& check_output(T& a, string s); // בודקת את אופרטור הפלט של הארגומנט שלה, משווה את התוצאה למחרוזת נתונה
-    template <typename T> T& check_function(T& a, T& b); //  בודקת פונקציה כללית כלשהי עם ארגומנט אחד.
+    TestCase(string s, ostream& cerr);
+    template <typename T> TestCase& check_equal(T& a, T& b); // בודקת אם שני הארגומנטים שלה שווים, אם לא - מדפיסה הודעת שגיאה
+    template <typename T> TestCase& check_different(T& a, T& b); // בודקת אם שני הארגומנטים שונים, כנ"ל
+    template <typename T> TestCase& check_output(T& a, string s); // בודקת את אופרטור הפלט של הארגומנט שלה, משווה את התוצאה למחרוזת נתונה
+    template <typename T> TestCase& check_function(T& a, T& b); //  בודקת פונקציה כללית כלשהי עם ארגומנט אחד.
     template <typename T> void print(); //  מדפיסה את תוצאות הבדיקה - כמה בדיקות עברו ונכשלו.
     
 };
 
+    TestCase(string s1, &ostream cerr){//constructor
+        s=s1;
+        
+    }
 
 
 
-    template <typename T> T& check_equal(T& a, T& b){
-        asser
+    template <typename T> TestCase& check_equal(T& a, T& b){
+        if(a!=b){
+           cerr << to_string( s +": Failure in test #1:"+ a + "should equal" + b + "!"); 
+        }
+
+        return *this;
+
+    }
+    
+    template <typename T> TestCase& check_different(T& a, T& b){
+        if(a==b){
+           cerr << to_string( s +": Failure in test #2:"+ a + "should be different" + b + "!"); 
+        }
+
+        return *this;
     }
