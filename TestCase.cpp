fdc31bc130709d56e1fab4216c639b723c1e3204
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2018-06-11 17:14:13 +0300

    Update TestCase.cpp

--- TestCase.cpp
+++ TestCase.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
-#include <ostream>
-using namespace std;
 
-TestCase::TestCase(string s1, &ostream cerr){//constructor
+#include "TestCase.hpp"
+
+TestCase::TestCase(string s1, ostream& cerr) : cerr(cerr.rdbuf()){//constructor
     s=s1;
     num_failure = counter = 0;
-    ostream os = cerr;
+
 }
 
 void TestCase::print(){
-    cerr << s << ":" << num_failure << "failed, " << counter-num_failure << "passed, " << counter << "total." << endl;
+    cerr << s << ": " << num_failure << " failed, " << counter-num_failure << " passed, " << counter << " total." << endl;
     cerr << "---" << endl;
 }
